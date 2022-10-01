fdb90935163548cbf1b484d6f5b1269d2c758d6e
Irfan <avoidirfan@gmail.com>
2022-10-01 12:15:01 +0600

    New service added

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/nothing')
+  getHelloNothing(): string {
+    return this.appService.getHelloNothing();
+  }
 }
